fcd4ee181cfd5ba40aa4fcff321ce73c4abd3bcd
David Sargento <david.sargento10@gmail.com>
2018-12-11 13:55:43 +0100

    day 1

---
+++ 1/main.py
@@ -0,0 +1,24 @@
+import parser
+import time
+
+my_parser = parser.Parser("./input.txt")
+count = 0
+found = 0
+history = []
+
+# While no duplicate is found
+while found == 0:
+    # Iterate through our sequence
+    for x in my_parser.sequence:
+        # Add the current number to the count
+        count += x
+        # If the current number is in our history
+        if count in history:
+            # We found it!
+            found = 1
+            break
+        # Append the current count to the history
+        history.append(count)
+
+print(history)
+print(count)
---
+++ 1/readme.txt
@@ -0,0 +1,9 @@
+Part 1 explanation:
+
+Pretty straightforward, the input file is parsed into a list of ints, then we apply the values using a for loop to get
+the answer
+
+Part 2 explanation:
+
+We have to find the first repetition, to do that we repeat the sequence until the values matches one stored in the
+history (it takes a while)
